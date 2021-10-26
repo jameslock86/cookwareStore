1290e72824ff561dca4ec2298c7d175b6b8c1f9b
james lockwood <ksjjl05@gmail.com>
2021-10-26 18:39:20 -0500

    working on the navigation

--- cookwarestore/components/navbar/navbar.jsx
+++ cookwarestore/components/navbar/navbar.jsx
@@ -1,43 +1,30 @@
 import React from 'react'
-import { Nav, Button } from 'react-bootstrap';
+import { Nav, Button, Container, NavDropdown, Form, FormControl, Toggle, NavBar } from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
 import Link from 'next/link'
 
-export default function Navbar() {
+export default function Navbars() {
   return (
-    <Nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div className="container-xl">
-    <Link href="/">
-      <a className="navbar-brand">CookWare Site</a>
-    </Link>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07XL" aria-controls="navbarsExample07XL" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
+    <Navbar sticky="top" expand="lg">
+    <Container fluid>
+      <Navbar.Brand href="#">Cookware & CookBooks</Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbarScroll" />
+      <Navbar.Collapse id="navbarScroll">
+        <Nav
+          className="me-auto my-2 my-lg-0"
+          style={{ maxHeight: '200px' }}
+          navbarScroll
+        >
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/shop">Shop</Nav.Link>
+          <Nav.Link href="/contact">Contact</Nav.Link>
+        </Nav>
+       
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
 
-    <div className="collapse navbar-collapse" id="navbarsExample07XL">
-      <ul className="navbar-nav mr-auto">
-        <li className="nav-item active">
-          <Link href="/">
-            <a className="nav-link">Home</a>
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link href="/about">
-            <a className="nav-link">About</a>
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link href="/contact">
-            <a className="nav-link">Contact</a>
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link href="/shop">
-            <a className="nav-link">Shop</a>
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </div>
-</Nav>
+
   )
 }
