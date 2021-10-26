b2b0b7d5a72b9099108670ff69086cd6d245bba7
ruro122020 <ruro122020@gmail.com>
2021-10-26 12:55:08 -0600

    get product by Id API complete

--- cookwarestore/pages/api/products/[productId].js
+++ cookwarestore/pages/api/products/[productId].js
@@ -1,17 +1,20 @@
 import { connectToDatabase } from "../../../lib/mongodb";
-
+import { ObjectId } from "mongodb";
 export default async (req, res) => {
-  const { db } = connectToDatabase();
-  console.log(req.query);
-  const { prodcutId } = req.query;
+  if (req.method === "GET") {
+    const { db } = await connectToDatabase();
+    const { productId } = req.query;
 
-  const product = db
-    .collection("products")
-    .find({
-      _id: prodcutId,
-    })
-    .toArray();
-
-  res.status(200).json(product);
-  //get product by id
+    if (productId.length === 24) {
+      const id = ObjectId(productId);
+      const product = await db.collection("products").findOne({
+        _id: id,
+      });
+      res.status(200).json(product);
+    } else {
+      res.send("Product does not exist");
+    }
+  } else {
+    res.send("Must be a GET request");
+  }
 };
