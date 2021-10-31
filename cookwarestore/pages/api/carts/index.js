e1080e2ee728fdc73c9478b65b62f3a8743f83a3
ruro122020 <ruro122020@gmail.com>
2021-10-31 02:58:15 -0600

    carts api

--- cookwarestore/pages/api/carts/index.js
+++ cookwarestore/pages/api/carts/index.js
@@ -7,27 +7,31 @@ export default async function cartsHandler(req, res) {
 
   switch (req.method) {
     case "POST":
-      return addProductToCartDB(req, res, db);
+      return addCart(req, res, db);
     default:
       return "GET and POST request only";
   }
 }
 
-const addProductToCartDB = (req, res, db) => {
+const addCart = (req, res, db) => {
   if (req.userId) {
-    //add product to cart
+    res.send(
+      "A cart with this user already exist. Please make a post request to cart ID"
+    );
   } else {
     //create userId
     const userId = new ObjectId();
-    //add product to cart
+    //create cart
     const cart = await db.collection(collection).insertOne(
       {
         userId: userId,
+        products: [],
       },
       (err, response) => {
         if (err) console.log("err", err);
+        //send userId to client
+        console.log("response", response);
       }
     );
-    //send userId to client
   }
 };
