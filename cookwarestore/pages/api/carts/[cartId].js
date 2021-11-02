79e3df4f63752dc8f2c1e30a93f72b71b4ae5cdb
ruro122020 <ruro122020@gmail.com>
2021-11-02 12:28:58 -0600

    cart api

--- cookwarestore/pages/api/carts/[cartId].js
+++ cookwarestore/pages/api/carts/[cartId].js
@@ -4,47 +4,7 @@ const collection = "carts";
 export default async function (req, res) {
   const { db } = await connectToDatabase();
   switch (req.method) {
-    case "PATCH":
-      return addProductToUsersCart(req, res, db);
-    case "DELETE":
-      return deleteProductsFromUsersCart(req, res, db);
     default:
       return "PATCH request only";
   }
 }
-
-const addProductToUsersCart = async (req, res, db) => {
-  try {
-    const { cartId } = req.query;
-    const { products } = req.body;
-    const id = ObjectId(cartId);
-    let results;
-    if (products.length > 0) {
-      for (let i = 0; i < products.length; i++) {
-        results = await db.collection(collection).update(
-          {
-            _id: id,
-          },
-          {
-            $push: {
-              products: {
-                title: products[i].title,
-                productId: products[i].productId,
-                quantity: products[i].quantity,
-              },
-            },
-          }
-        );
-      }
-
-      res.status(200).json({
-        msg: "Product has been added to user's cart",
-        result: results.result.ok,
-      });
-    }
-  } catch (err) {
-    console.log("err", err);
-  }
-};
-
-const deleteProductsFromUsersCart = (req, res, db) => {};
--- cookwarestore/pages/api/carts/index.js
+++ cookwarestore/pages/api/carts/index.js
@@ -6,49 +6,121 @@ export default async function cartsHandler(req, res) {
   const { db } = await connectToDatabase();
 
   switch (req.method) {
-    case "POST":
-      return addCart(req, res, db);
+    // case "POST":
+    //   return addCart(req, res, db);
+    case "PATCH":
+      return updateUsersCart(req, res, db);
+    case "DELETE":
+      return deleteProductsFromUsersCart(req, res, db);
     default:
-      return "POST request only";
+      return "No cases found";
   }
 }
 
-const addCart = async (req, res, db) => {
-  try {
-    if (req.body.userId) {
-      const userId = ObjectId(req.body.userId);
-      //get user's cart
-      const usersCart = await db.collection(collection).findOne({ userId });
-      res.json({
-        msg: "Cart already exist for user. Please make a PATCH request to users cart _id ",
-        usersCart,
-      });
-    } else {
-      //create userId
-      const userId = new ObjectId();
-      //create cart
-      await db.collection(collection).insertOne(
-        {
-          userId: userId,
-          products: req.body.products || [],
-        },
-        (err, response) => {
-          if (err) console.log("err", err);
-          //send userId to client
-          console.log("response", response.ops);
-          res
-            .status(200)
-            .json({ msg: "cart has been added", cart: response.ops });
-        }
-      );
+// const addCart = async (req, res, db) => {
+//   try {
+//     if (req.body.userId) {
+//       const userId = ObjectId(req.body.userId);
+//       //get user's cart
+//       const usersCart = await db.collection(collection).findOne({ userId });
+//       res.json({
+//         msg: "Cart already exist for user. Please make a PATCH request to users cart _id ",
+//         usersCart,
+//       });
+//     }
+//   } catch (err) {
+//     console.log("err", err);
+//   }
+// };
 
-      //create user in users collection
+/*task to be do: 
+  1. check to see if product has already been added to user's cart
+  2. check if quantity has changed
+  3. update products quantity in products colletion and set a timer to reverse the product quantity if user doesn't purchase product after a 
+     certain amount of time
+*/
+const updateUsersCart = async (req, res, db) => {
+  try {
+    const { userId, products } = req.body;
+    const uid = ObjectId(userId);
+    if (req.body.userId) {
+      //check if a cart already exist for user
+      //if cart doesn't exist, add cart with products by using update function
+      //if cart exist, check if product exist in cart
+      //if product exist, check quantity
+      //if quantity is the same, do nothing
+      //if qantity is different, update qantity
+    } else {
+      res.json({ msg: "please include user's id" });
+    }
+  } catch (err) {
+    console.log("err", err);
+  }
+};
+const addProductToUsersCart = async (req, res, db) => {
+  try {
+    const { products, userId } = req.body;
+    const id = ObjectId(userId);
+    if (products.length > 0) {
+      const isProductInCart = checkProductInUsersCart(id, products, db);
+      if (isProductInCart) {
+        console.log("isProductInCart", isProductInCart);
+        //compare quantity
+
+        //update products quantity if needed
+        // updateProductsInventory(products);
+        res.status(200).json({ msg: "quantity updated" });
+      }
+      // else {
+      //   //add product to cart
+      //   addProductToCart(userId, products, db);
+      //   //update products collection quantity
+      //   updateProductsInventory(products);
+      //   res.status(200).json({ msg: "Product has been added to user's cart" });
+      // }
     }
   } catch (err) {
     console.log("err", err);
   }
 };
 
+const deleteProductsFromUsersCart = async (req, res, db) => {};
+
+/* Helper Functions */
+const checkProductInUsersCart = async (uid, userProducts, db) => {
+  let productResults;
+  const cart = await db.collection(collection).findOne({ userId: uid });
+  if (cart) {
+    const { products, userId, _id } = cart;
+    for (let i = 0; i < userProducts.length; i++) {
+      productResults = products.filter((product) => {
+        return product === userProducts[i];
+      });
+    }
+    console.log("productResults", productResults);
+  } else {
+    console.log("cart", cart);
+  }
+};
+
+const addProductToCart = async (product) => {
+  const { result } = await db.collection(collection).update(
+    {
+      userId: id,
+    },
+    {
+      $push: {
+        products: {
+          productId: product.productId,
+          quantity: product.quantity,
+        },
+      },
+    }
+  );
+  return result.ok;
+};
+
+const updateProductsInventory = async (products) => {};
 /*
 *Edge Cases* 
 1. If user doesn't exist create a session with user id
