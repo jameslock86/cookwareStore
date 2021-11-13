758aaadc3c400cedc09da5b86ceb01929a1682ec
ruro122020 <ruro122020@gmail.com>
2021-11-13 16:21:27 -0700

    testing api routes

--- cookwarestore/lib/db.js
+++
@@ -1,9 +0,0 @@
-import clientPromise from "../lib/mongodb";
-export const getDB = async () => {
-  const db = await clientPromise;
-  const dbState = await db.db("cookwarestore");
-  // console.log("inside getdb function", dbState);
-  return dbState;
-};
-
-module.exports = { getDB };
